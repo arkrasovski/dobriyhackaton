acd58e9844f39b0a810f07c152c8c3f25ef980ea
Nikita <nikita.reshetnikov9@gmail.com>
2021-05-15 17:45:59 +0300

    docs: add css-styles

--- dobriy-react-app/src/components/Exhibition.js
+++ dobriy-react-app/src/components/Exhibition.js
@@ -23,10 +23,16 @@ class Picture extends React.Component {
           <div class="picture">
             <img class="picture-image" src={picture.picture}></img>
             <div class="picture-info">
-              <h3>Title: {picture.title}</h3>
-              <h3>Author: {picture.author}</h3>
-              <h3>Year: {picture.year}</h3>
-              <h3>Description: {picture.description}</h3>
+              <span class="picture-title">{picture.title}</span>
+              <span class="info">
+                <span class="bold">Author:</span> {picture.author}
+              </span>
+              <span class="info">
+                <span class="bold">Year:</span> {picture.year}
+              </span>
+              <span class="info">
+                <span class="bold">Description:</span> {picture.description}
+              </span>
             </div>
           </div>
         ))}
