27f29d111c580be5715275f5866fb8fd5644ec4a
arkrasovski <arkrasovski1996@gmail.com>
2021-05-15 16:58:29 +0300

    feat: add header

--- dobriy-react-app/src/App.js
+++ dobriy-react-app/src/App.js
@@ -1,19 +1,8 @@
 import "./App.css";
-import "./LastSeen.css";
-import "./Recommendations.css";
 import React from "react";
-//import WeeeklyFilms from "./components/weeklyFilms";
 import Map from "./components/Map";
-// import User from "./components/User";
-import logo from "./images/logo.svg";
 import search from "./images/Search.svg";
-import user from "./images/account.svg";
 
-import twitter from "./images/twitter.svg";
-import facebook from "./images/facebook.svg";
-import vk from "./images/vk.svg";
-//import LastSeen from "./components/LastSeen";
-//import Recommendations from "./components/Recommendations";
 import {
   BrowserRouter as Router,
   Switch,
@@ -31,10 +20,9 @@ function App() {
       <div className="App">
         <header>
           <div className="leftbar">
-            <div className="logo">
-              <a> <Link to="/"><img src={logo}></img></Link></a>
-            </div>
+            <span><Link to="/">Dobriy museum</Link></span>
             <ul>
+              
               {/* <li>
                 <Link to="/events/monday">Timetable</Link>
               </li> */}
@@ -47,9 +35,8 @@ function App() {
             </ul>
           </div>
           <div className="rightbar">
-            <input placeholder="Find the film..." type="text"></input>
+            <input type="text"></input>
             <img className="lupa" src={search}></img>
-            <img src={user}></img>
           </div>
         </header>
 
@@ -81,19 +68,6 @@ function App() {
        
         </section>
 
-        <footer>
-          <div className="footerText">
-            <span>Join us: </span>
-            <img src={twitter}></img>
-            <img src={facebook}></img>
-            <img src={vk}></img>
-          </div>
-          <div className="footerSearch">
-            <p>News</p>
-            <input placeholder="Enter your email" type="text"></input>
-            <button>Subscribe</button>
-          </div>
-        </footer>
       </div>
     </Router>
   );
